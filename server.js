1262f048afb66a9ed24caca3f2794be3d93c5052
Stanislau Tukhto <s.tukhto.vironit@vironit.ru>
2018-09-05 16:50:23 +0300

    Implemented server.js with routes params

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const http = require('http');
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-  res.writeHead(200, {'Content-type': 'text/html'});
-  res.end('');
-});
+	if (req.url.match(/\.(html|css|js|png)$/)){
 
-server.listen(3000, () => console.log('server listening on port 3000'));
+	} else if (req.url === '/') {
+
+	} else if (req.url.startsWith('/search')){
+
+	} else {
+
+	}
+
+	res.writeHead(200, {'Content-type': 'text/html'});
+	res.end('');
+}).listen(3000, () => console.log('server listening on port 3000'));
